d142ea61b3525ce55f529895d5b4175e0403580b
milin-fr <44909267+milin-fr@users.noreply.github.com>
2019-09-06 15:20:43 +0200

    Add files via upload

---
+++ tkinter_scrollable_list_with_click_event.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+master = Tk()
+
+def onselect(evt):
+    # Note here that Tkinter passes an event object to onselect()
+    w = evt.widget
+    index = int(w.curselection()[0])
+    value = w.get(index)
+    print('You selected item %d: "%s"' % (index, value))
+
+scrollbar = Scrollbar(master)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+listbox = Listbox(master, yscrollcommand=scrollbar.set)
+for i in range(1000):
+    listbox.insert(END, str(i))
+listbox.pack(side=LEFT, fill=BOTH)
+listbox.bind('<<ListboxSelect>>', onselect)
+
+scrollbar.config(command=listbox.yview)
+
+mainloop()
